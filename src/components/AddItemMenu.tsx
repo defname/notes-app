6bfca36589919297e7ec649e1644700ecb79fa8e
defname <bodo.akdeniz@gmail.com>
2024-12-19 16:40:39 +0100

    change NavLink to Link to solve base url problem

--- src/components/AddItemMenu.tsx
+++ src/components/AddItemMenu.tsx
@@ -1,7 +1,8 @@
-import { ActionIcon, Affix, Menu, NavLink, rem } from "@mantine/core"
+import { ActionIcon, Affix, Menu, rem } from "@mantine/core"
 import { IconPlus } from "@tabler/icons-react"
 
 import Notes from "../lib/notes"
+import { Link } from "react-router"
 
 interface AddItemMenuProps {
     parentId?: string
@@ -23,7 +24,10 @@ export default function AddItemMenu({ parentId }: AddItemMenuProps) {
           <Menu.Label>Neues Item</Menu.Label>
           {
           Notes.supportedTypes().map(type => (
-              <NavLink key={ type.id } href={ `create/${type.id}${parentIdUrlStr}` } leftSection={ <type.icon style={{ width: rem(14), height: rem(14) }} /> } label={ type.text } />
+              <Link key={type.id} to={`/create/${type.id}${parentIdUrlStr}`}>
+                <Menu.Item leftSection={ <type.icon style={{ width: rem(14), height: rem(14) }} /> }>{type.text}</Menu.Item>
+              </Link>
+              
           ))
           }
       </Menu.Dropdown>
