958c40c7d6935b8d5399274c9d8b97ce84e27d8e
defname <bodo.akdeniz@gmail.com>
2024-12-23 19:45:46 +0100

    refactor: Add documentation for filtering and searching hooks and optimize useSearch with useMemo

--- src/hooks/filter.ts
+++ src/hooks/filter.ts
@@ -1,14 +1,25 @@
+/**
+ * Module to provide filtering, searching, and sorting functionalities for notes.
+ * Uses Fuse.js for searching and filtering.
+ */
+
 import { DBItem } from "../lib/db"
 import Fuse from "fuse.js"
 import { NotesManager } from "../lib/notes"
-import { useEffect, useState } from "react"
-
+import { useEffect, useMemo, useState } from "react"
 
 export interface FilterOpts {
     searchStr: string
     filterTypes: string[]
+    orderDesc?: boolean
 }
 
+/**
+ * Custom hook to prefilter items based on filter options.
+ * @param items The list of items to filter
+ * @param filter The filter options
+ * @returns The filtered list of items
+ */
 function usePrefilter(items: DBItem[], filter: FilterOpts) {
     const [filtered, setFiltered] = useState<DBItem[]>([])
     useEffect(() => {
@@ -18,16 +29,21 @@ function usePrefilter(items: DBItem[], filter: FilterOpts) {
     return filtered
 }
 
+/**
+ * Custom hook to search items based on a search string.
+ * @param items The list of items to search
+ * @param searchStr The search string
+ * @returns The list of items that match the search string
+ */
 export function useSearch(items: DBItem[], searchStr: string) {
     const [results, setResults] = useState<DBItem[]>([])
-    const searchables = items.map(item => ({ item: item, searchable: NotesManager.asSearchable(item) }))
+    const searchables = useMemo(() => items.map(item => ({ item: item, searchable: NotesManager.asSearchable(item) })), [items])
     const fuse = new Fuse(searchables, {
         includeScore: true,
         keys: ["searchable"]
     })
 
     useEffect(() => {
-        console.log("searching")
         if (searchStr === "") {
             setResults(items)
             return
@@ -38,8 +54,15 @@ export function useSearch(items: DBItem[], searchStr: string) {
     return results
 }
 
+/**
+ * Custom hook to filter items based on filter options.
+ * @param items The list of items to filter
+ * @param filter The filter options
+ * @returns The filtered and searched list of items
+ */
 export function useFilter(items: DBItem[], filter: FilterOpts) {
     const prefilteredItems = usePrefilter(items, filter)
+    //const sortedItems = useSort(prefilteredItems, "lastChange", filter.orderDesc || false)
     const results = useSearch(prefilteredItems, filter.searchStr)
 
     return results
