cceff085eb5aaf26cc3bf545bc99e8808df3c864
defname <bodo.akdeniz@gmail.com>
2024-12-26 01:12:24 +0100

    feat: add defaultMenu prop and handle click outside to reset active menu in BubbleMenu

--- src/components/BubbleMenu/components/BubbleMenu.tsx
+++ src/components/BubbleMenu/components/BubbleMenu.tsx
@@ -1,20 +1,24 @@
 import { useBubbleMenuControls } from "../hooks"
 import { BubbleMenuContext } from "../context"
 import { Affix } from "@mantine/core"
-import { useEffect, useMemo } from "react"
+import { useClickOutside } from "@mantine/hooks"
 
 interface BubbleMenuOwnProps<C extends React.ElementType> {
   component?: C
+  defaultMenu?: number
 }
 
 type BubbleMenuProps<C extends React.ElementType> = BubbleMenuOwnProps<C> & Omit<React.ComponentProps<C>, keyof BubbleMenuOwnProps<C>>
 
 const __DEFAULT_COMPONENT__ = Affix
 
-function BubbleMenu<C extends React.ElementType = typeof __DEFAULT_COMPONENT__>({ children, component, ...compProps }: BubbleMenuProps<C>) {
+function BubbleMenu<C extends React.ElementType = typeof __DEFAULT_COMPONENT__>({ children, component, defaultMenu, ...compProps }: BubbleMenuProps<C>) {
   const Component = component || __DEFAULT_COMPONENT__
   const filteredChildren = Array.isArray(children) ? children.filter((child: any) => child !== undefined) : [children]
   const [activeMenu, setActiveMenu, rotateActiveMenu] = useBubbleMenuControls(Array.isArray(filteredChildren) ? filteredChildren.length : 1)
+  const clickOutsideRef = useClickOutside(() => {
+    setActiveMenu(defaultMenu || 0)
+  })
   const context = {
     activeMenu,
     setActiveMenu,
@@ -24,7 +28,7 @@ function BubbleMenu<C extends React.ElementType = typeof __DEFAULT_COMPONENT__>(
 
   return (<>{
     <BubbleMenuContext.Provider value={ context }>
-      <Component { ...compProps }>
+      <Component ref={ clickOutsideRef } { ...compProps }>
         { filteredChildren[activeMenu] }
       </Component>
     </BubbleMenuContext.Provider>
